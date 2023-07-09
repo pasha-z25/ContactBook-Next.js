4df4e76b05be02fcb2347a13bbda586394c5673c
Pasha Zubak <pashazubak@gmail.com>
2023-07-09 16:21:27 +0300

    add TypeScript

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 const path = require('path')
 
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const Config = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
 }
+
+module.exports = Config;
+
--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/index.scss';
-
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
